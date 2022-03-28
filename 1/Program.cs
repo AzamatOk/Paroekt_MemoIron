71a5f1cd5d290618ff29640dbdcf32552eb75218
46552 <46552@10-600-05>
2022-03-28 10:59:54 +0300

    Добавьте файлы проекта.

---
+++ 1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _1
+{
+    class Program
+    {
+        public static int GetNODModul(int val1, int val2)
+        {
+            while ((val1 != 0) && (val2 != 0))
+            {
+                if (val1 > val2)
+                    val1 %= val2;
+                else
+                    val2 %= val1;
+            }
+            return Math.Max(val1, val2);
+        }
+    }
+}
